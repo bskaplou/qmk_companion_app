d5daa80aebe9463d512d65aeca8f521481c3940d
BORIS KAPLUNOVSKY <bskaplou@gmail.com>
2025-10-01 12:49:45 +0300

    black

--- unicode_keymap/generator.py
+++ unicode_keymap/generator.py
@@ -11,7 +11,7 @@ current_dir = Path(__file__).parent
 emojilist_path = os.path.join(current_dir, "emojilist.txt")
 
 emojis = {}
-with open(emojilist_path, 'r') as f:
+with open(emojilist_path, "r") as f:
     for line in f.readlines():
         sym, sc = line.split(" ")
         emojis[sym] = sc[6:-8]
@@ -20,32 +20,32 @@ if len(sys.argv) == 1:
     print(json.dumps(emojis, indent=4, ensure_ascii=False))
 else:
 
-# TARGET FORMAT
-#
-# enum unicode_keycodes {
-#   GRINNING_FACE = SAFE_START,
-#   FROWNING_FACE,
-# };
-# 
-# const char* unisymbols[][2] = {
-#     {":-)", (char*) U"\U0001F600"},
-#     {"'=D", (char*) U"\U0001F605"},
-#     {">:)", (char*) U"\U0001F605"},
-# };
+    # TARGET FORMAT
+    #
+    # enum unicode_keycodes {
+    #   GRINNING_FACE = SAFE_START,
+    #   FROWNING_FACE,
+    # };
+    #
+    # const char* unisymbols[][2] = {
+    #     {":-)", (char*) U"\U0001F600"},
+    #     {"'=D", (char*) U"\U0001F605"},
+    #     {">:)", (char*) U"\U0001F605"},
+    # };
 
-#    "customKeycodes": [
-#	      {
-#            "name": "Grinning face",
-#	          "title": "Grinning face",
-#	          "shortName": "UNC_GF"
-#	      },
-#	      {
-#            "name": "Sweating face",
-#	          "title": "Grinning face with sweat",
-#	          "shortName": "UNC_SF"
-#	      }
-#    ],
-    process_function = '''
+    #    "customKeycodes": [
+    # 	      {
+    #            "name": "Grinning face",
+    # 	          "title": "Grinning face",
+    # 	          "shortName": "UNC_GF"
+    # 	      },
+    # 	      {
+    #            "name": "Sweating face",
+    # 	          "title": "Grinning face with sweat",
+    # 	          "shortName": "UNC_SF"
+    # 	      }
+    #    ],
+    process_function = """
 #define SAFE_START QK_KB_0 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -60,7 +60,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return true;
   }
 }
-'''
+"""
 
     unicode_keycodes = "enum unicode_keycodes {\n"
     unisymbols = "const char* unisymbols[][2] = {\n"
@@ -68,12 +68,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     symbols = set(sys.argv[1:])
     for idx, symbol in enumerate(symbols):
-        symbol_bytes = list(reversed(bytes(symbol, 'utf32')[4:]))
+        symbol_bytes = list(reversed(bytes(symbol, "utf32")[4:]))
         symbol_hex = []
         for byte in symbol_bytes:
             symbol_hex.append("%0.2X" % byte)
-        
-        symbol_char = "U" + ''.join(symbol_hex)
+
+        symbol_char = "U" + "".join(symbol_hex)
 
         title = unicodedata.name(symbol).title()
         constant = title.upper().replace(" ", "_")
@@ -83,7 +83,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         else:
             fallback = ":" + t.lower().replace(" ", "_") + ":"
 
-        short_name = ''.join(symbol_hex).lstrip('0')
+        short_name = "".join(symbol_hex).lstrip("0")
         name = "U+" + short_name
 
         if idx == 0:
@@ -91,14 +91,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         else:
             unicode_keycodes = unicode_keycodes + f"    {constant},\n"
 
-        unisymbols = unisymbols + f"    {{\"{fallback}\", (char*) U\"\\{symbol_char}\"}},\n"
+        unisymbols = unisymbols + f'    {{"{fallback}", (char*) U"\\{symbol_char}"}},\n'
 
-        vial_keycodes = vial_keycodes + f"        {{\n            \"name\": \"{name}\",\n            \"title\": \"{title}\",\n            \"shortName\": \"{short_name}\"\n        }},\n"
+        vial_keycodes = (
+            vial_keycodes
+            + f'        {{\n            "name": "{name}",\n            "title": "{title}",\n            "shortName": "{short_name}"\n        }},\n'
+        )
 
     unicode_keycodes = unicode_keycodes + "};\n"
     unisymbols = unisymbols + "};\n"
     vial_keycodes = vial_keycodes[0:-2] + "\n    ],"
-
 
     print("===============  put following code into keymap.c ===============")
     print(unicode_keycodes)
